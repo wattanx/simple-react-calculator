6871f0f3514b6eee44dad07a00d03000f9088f2f
ryotawattan <ex-j.s.b-0716@outlook.jp>
2021-06-05 20:52:06 +0900

    減算を追加

--- src/components/Calculator/Calculator.ts
+++ src/components/Calculator/Calculator.ts
@@ -25,27 +25,34 @@ export class Calculator {
             this.prevOperator = operator;
             return this.prevInputValue;
         }
-        
-        switch(operator){
-            case Operator.addition:
-                this.prevInputValue = this.engine.add(this.prevInputValue, value);
-                this.prevOperator = operator;
-                return this.prevInputValue;
-            case Operator.equal:
-                this.prevInputValue = this.calculateInner(value, this.prevOperator);
-                this.prevOperator = operator;
-                return this.prevInputValue;
-            default:
-                return '';
+
+        if (operator === Operator.equal) {
+            this.prevInputValue = this.calculateInner(value, this.prevOperator);
+            this.prevOperator = operator;
+            return this.prevInputValue;
         }
+
+        return this.calculateInner(value, operator);
+    }
+
+    private add(value: string, operator: Operator): string {
+        this.prevInputValue = this.engine.add(this.prevInputValue, value);
+        this.prevOperator = operator;
+        return this.prevInputValue;
+    }
+
+    public subtract(value: string, operator: Operator): string {
+        this.prevInputValue = this.engine.subtract(this.prevInputValue, value);
+        this.prevOperator = operator;
+        return this.prevInputValue;
     }
 
     private calculateInner(value: string, operator: Operator): string {
         switch(operator){
             case Operator.addition:
-                this.prevInputValue = this.engine.add(this.prevInputValue, value);
-                this.prevOperator = operator;
-                return this.prevInputValue;
+                return this.add(value, operator);
+            case Operator.subtraction: 
+                return this.subtract(value, operator);
             default:
                 return '';
         }
