210da6b931eeb02df0962be65e58ae5b7e80487f
ryotawattan <ex-j.s.b-0716@outlook.jp>
2021-06-05 15:47:04 +0900

    計算ロジックを追加

---
+++ src/components/Calculator/Engine.ts
@@ -0,0 +1,31 @@
+export class Engine {
+    public add(prevValue: string, value: string): string {
+        return (parseFloat(prevValue) + parseFloat(value)).toString();
+    }
+
+    public subtract(prevValue: string, value: string): string {
+        return (parseFloat(prevValue) - parseFloat(value)).toString();
+    }
+
+    public multiply(prevValue: string, value: string): string {
+        return (parseFloat(prevValue) * parseFloat(value)).toString();
+    }
+
+    public divide(prevValue: string, value: string): string {
+        if (this.isDivisionByZero(prevValue, value)) { return 'Error'; }
+        return (parseFloat(prevValue) / parseFloat(value)).toString();
+    }
+
+    public parcentage(value: string): string {
+        return (parseFloat(value) / 100).toString();
+    }
+
+    public changeSign(value: string): string {
+        return parseFloat(value) === 0 ? '0' : (parseFloat(value) * -1).toString();
+    }
+
+    private isDivisionByZero(prevValue: string, value: string): boolean {
+        const num = parseFloat(value);
+        return (parseFloat(prevValue) === 0 && num === 0) || num === 0;
+    }
+}
