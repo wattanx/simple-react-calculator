e2da7bb22d574472e5ebd7ed1dc733f85cf27ec2
ryotawattan <ex-j.s.b-0716@outlook.jp>
2021-06-06 16:39:28 +0900

    数字表示用コンポーネントを追加

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,12 +1,41 @@
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
-export const Display: React.FC = (props) => {
+type DisplayProps = {
+    value: string;
+}
+
+export const Display: React.FC<DisplayProps> = (props) => {
+    const divRef = useRef<HTMLDivElement>();
+    const spanRef = useRef<HTMLSpanElement>();
+
+    const getFontSize = () => {
+        return window.getComputedStyle(divRef.current, '').getPropertyValue('font-size');
+    }
+
+    const reduceFontSize = () => {
+        if (divRef.current.clientWidth > spanRef.current.clientWidth + 40) { return; }
+
+        divRef.current.style.fontSize = parseFloat(getFontSize()) - 10 + 'px';
+        reduceFontSize();
+    }
+
+    const resetFontSize = () => {
+        divRef.current.style.fontSize = '14vmin';
+    }
+
+    useEffect(() => {
+        resetFontSize();
+        reduceFontSize();
+    }, [props.value]);
+
     return (
-        <StyledDiv>
-            <StyledSpan></StyledSpan>
+        <StyledDiv ref={divRef}>
+            <StyledSpan ref={spanRef}>{props.value}</StyledSpan>
         </StyledDiv>
     );
 }
+
 
 const StyledDiv = styled.div`
     width: 100%;
