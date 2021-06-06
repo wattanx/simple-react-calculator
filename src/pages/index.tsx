55004346c7eb0013b914da9dedb0688b4335cd70
ryotawattan <ex-j.s.b-0716@outlook.jp>
2021-06-06 16:40:25 +0900

    bodyタグのスタイルを変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
 import { ReactCalculator } from '@src/components/ReactCalculator'
 import Link from 'next/link'
+import { createGlobalStyle } from 'styled-components'
 import Layout from '../components/Layout'
 
 const IndexPage = () => (
-  <ReactCalculator />
+  <>
+    <GlobalStyle />
+    <ReactCalculator />
+  </>
 )
 
 export default IndexPage
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    margin: 0;
+    padding: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`
