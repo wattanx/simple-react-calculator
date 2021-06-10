fae8761d32c84de455ecb582e0d305da7884fabe
ryotawattan <ex-j.s.b-0716@outlook.jp>
2021-06-10 22:37:55 +0900

    Headを追加

---
+++ site.config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    siteMeta: {
+        title: 'react-calculator',
+        description: 'Calculator built with React',
+    },
+    siteRoot: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000',
+    headerLinks: [],
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
+import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
     return (
-        <Component {...pageProps}/>
-    )
+        <>
+            <Head>
+                <link
+                    rel="icon"
+                    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🐱‍👤</text></svg>"
+                ></link>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
