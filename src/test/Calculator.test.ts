d63d2938478ee84a8ce771439a9c671bafb0d673
ryotawattan <ex-j.s.b-0716@outlook.jp>
2021-07-03 21:37:08 +0900

    jestとUnitTestの追加

---
+++ src/test/Calculator.test.ts
@@ -0,0 +1,79 @@
+import { Calculator, Operator } from "../components/Calculator/Calculator";
+
+test("addition Test", () => {
+  const calculator = new Calculator();
+  expect(calculator.calculate("1")).toBe("1");
+  expect(calculator.calculate("1")).toBe("11");
+  expect(calculator.calculate("+")).toBe("11");
+  expect(calculator.calculate("3")).toBe("3");
+  expect(calculator.calculate("=")).toBe("14");
+});
+
+test("subtract Test", () => {
+  const calculator = new Calculator();
+  expect(calculator.calculate("1")).toBe("1");
+  expect(calculator.calculate("0")).toBe("10");
+  expect(calculator.calculate("-")).toBe("10");
+  expect(calculator.calculate("1")).toBe("1");
+  expect(calculator.calculate("-")).toBe("9");
+});
+
+test("multiply Test", () => {
+  const calculator = new Calculator();
+  expect(calculator.calculate("1")).toBe("1");
+  expect(calculator.calculate("2")).toBe("12");
+  expect(calculator.calculate(Operator.multiplication)).toBe("12");
+  expect(calculator.calculate("2")).toBe("2");
+  expect(calculator.calculate(Operator.multiplication)).toBe("24");
+});
+
+test("divide Test", () => {
+  const calculator = new Calculator();
+  expect(calculator.calculate("1")).toBe("1");
+  expect(calculator.calculate("0")).toBe("10");
+  expect(calculator.calculate(Operator.division)).toBe("10");
+  expect(calculator.calculate("5")).toBe("5");
+  expect(calculator.calculate(Operator.division)).toBe("2");
+});
+
+test("divisionByZero Test1", () => {
+  const calculator = new Calculator();
+  expect(calculator.calculate("1")).toBe("1");
+  expect(calculator.calculate("0")).toBe("10");
+  expect(calculator.calculate(Operator.division)).toBe("10");
+  expect(calculator.calculate("0")).toBe("0");
+  expect(calculator.calculate("=")).toBe("Error");
+});
+
+test("divisionByZero Test2", () => {
+  const calculator = new Calculator();
+  expect(calculator.calculate("0")).toBe("0");
+  expect(calculator.calculate(Operator.division)).toBe("0");
+  expect(calculator.calculate("0")).toBe("0");
+  expect(calculator.calculate("=")).toBe("Error");
+});
+
+test("percentage Test", () => {
+  const calculator = new Calculator();
+  expect(calculator.calculate("1")).toBe("1");
+  expect(calculator.calculate("0")).toBe("10");
+  expect(calculator.calculate(Operator.percentage)).toBe("0.1");
+});
+
+test("ChangeSign Test", () => {
+  const calculator = new Calculator();
+  expect(calculator.calculate("1")).toBe("1");
+  expect(calculator.calculate("0")).toBe("10");
+  expect(calculator.calculate(Operator.sign)).toBe("-10");
+});
+
+test("clear Test", () => {
+  const calculator = new Calculator();
+  expect(calculator.calculate("1")).toBe("1");
+  expect(calculator.calculate("0")).toBe("10");
+  expect(calculator.calculate(Operator.division)).toBe("10");
+  expect(calculator.calculate("2")).toBe("2");
+  expect(calculator.calculate(Operator.clear)).toBe("0");
+  expect(calculator.calculate("2")).toBe("2");
+  expect(calculator.calculate(Operator.equal)).toBe("5");
+});
---
+++ src/test/Engine.test.ts
@@ -0,0 +1,14 @@
+import { Engine } from "../components/Calculator/Engine";
+
+test("Engine Test", () => {
+  const engine = new Engine();
+  expect(engine.add("135", "100")).toBe("235");
+  expect(engine.subtract("135", "100")).toBe("35");
+  expect(engine.multiply("100", "400")).toBe("40000");
+  expect(engine.divide("100", "25")).toBe("4");
+  expect(engine.divide("0", "0")).toBe("Error");
+  expect(engine.divide("30", "0")).toBe("Error");
+  expect(engine.percentage("30")).toBe("0.3");
+  expect(engine.changeSign("-5")).toBe("5");
+  expect(engine.changeSign("5")).toBe("-5");
+});
