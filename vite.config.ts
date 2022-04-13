463163775f5361d2be267f187dacd613ffa90366
wattanx <wattan.dev@gmail.com>
2022-04-13 22:58:47 +0900

    feat: add vite and setting

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import { peerDependencies } from "./package.json";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+export default defineConfig({
+  plugins: [react({ jsxRuntime: "classic" })],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      fileName: (ext) => `index.${ext}.js`,
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
+    },
+    target: "esnext",
+    sourcemap: true,
+  },
+});
